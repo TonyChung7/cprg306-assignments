19d544eb95bfecfeffd92f71050ddaa10fb4d116
TonyChung7 <shihwei.chung@edu.sait.ca>
2026-01-21 22:56:58 -0700

    Initial Commit

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Name: Shih Wei Chung</p>
+      <p>
+        GitHub: <Link href="">My GitHub Repository</Link>
+      </p>
+    </div>
+  );
+}
