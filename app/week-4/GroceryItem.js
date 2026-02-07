c6a1bde8e2d40be3160c0e8ffc6886e052bc530b
TonyChung7 <shihwei.chung@edu.sait.ca>
2026-02-07 15:58:56 -0700

    update h1 title display for dark mode

--- app/week-4/GroceryItem.js
+++ app/week-4/GroceryItem.js
@@ -1,7 +1,8 @@
 export default function GroceryItem({ name, quantity, category }) {
   return (
     <li
-      className="p-4 m-3 bg-white text-slate-900 border-l-4 border-orange-500
+      className="p-4 m-3 bg-white text-slate-900 border-l-4 border-orange-500 border-b-2
+      
     rounded-r-lg hover:shadow-lg hover:bg-stone-300 transition-colors duration-300 "
     >
       <p className="text-lg capitalize">{name}</p>
--- app/week-4/page.js
+++ app/week-4/page.js
@@ -3,7 +3,7 @@ import GroceryItemList from "./GroceryItemList";
 export default function Page() {
   return (
     <main className="bg-stone-100 p-8 min-h-screen">
-      <h1 className="text-4xl font-extrabold text-center mb-6">
+      <h1 className="text-4xl font-extrabold text-center mb-6 text-slate-950">
         Shopping List
       </h1>
       <GroceryItemList />
