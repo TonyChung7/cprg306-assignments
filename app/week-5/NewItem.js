9fc5f901487b486f8291f720757652ad7f543705
TonyChung7 <shihwei.chung@edu.sait.ca>
2026-02-23 15:33:12 -0700

    Add missing  id and htmlFor attributes and support dark mode viewing

--- app/week-5/NewItem.js
+++ app/week-5/NewItem.js
@@ -1,10 +1,10 @@
 "use client";
 import { useState } from "react";
-
+import { CategoryOptions } from "./CategoryOptions";
 export default function NewItem() {
   const [itemName, setItemName] = useState("");
   const [itemQuantity, setItemQuantity] = useState(1);
-  const [itemCategory, setItemCategory] = useState("produce");
+  const [itemCategory, setItemCategory] = useState(CategoryOptions[0]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -22,15 +22,20 @@ export default function NewItem() {
 
     setItemName("");
     setItemQuantity(1);
-    setItemCategory("produce");
+    setItemCategory(CategoryOptions[0]);
   };
 
   return (
     <form
-      className="bg-gray-200 p-4 items-center rounded-lg w-[80%] max-w-lg mx-auto flex flex-col gap-4"
+      className="bg-gray-200 text-black p-4 items-center rounded-lg w-[80%] max-w-lg mx-auto flex flex-col gap-4"
       onSubmit={handleSubmit}
     >
+      <label htmlFor="item-name" className="sr-only">
+        Item Name
+      </label>
       <input
+        id="item-name"
+        name="item-name"
         className="text-center p-2 rounded-md border-2 border-gray-300 w-full"
         type="text"
         value={itemName}
@@ -38,10 +43,13 @@ export default function NewItem() {
         required
         placeholder="Item Name"
       />
+
       <div className="flex w-full">
         <div className="flex flex-1">
-          <p>Quantity:</p>
+          <label htmlFor="item-quantity">Quantity:</label>
           <input
+            id="item-quantity"
+            name="item-quantity"
             className="flex-1 text-center border-r-2 border-gray-300"
             type="number"
             value={itemQuantity}
@@ -51,7 +59,8 @@ export default function NewItem() {
             placeholder="Quantity"
           />
         </div>
-        <select
+
+        {/* <select
           className="flex-1 text-center bg-gray-200 "
           value={itemCategory}
           onChange={(e) => setItemCategory(e.target.value)}
@@ -68,6 +77,22 @@ export default function NewItem() {
           <option value="snacks">Snacks</option>
           <option value="household">Household</option>
           <option value="other">Other</option>
+        </select> */}
+        <label htmlFor="item-category" className="sr-only  ">
+          Category Options
+        </label>
+        <select
+          name="item-category"
+          id="item-category"
+          className="capitalize flex-1 text-center bg-gray-200"
+          value={itemCategory}
+          onChange={(e) => setItemCategory(e.target.value)}
+        >
+          {CategoryOptions.map((category) => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
         </select>
       </div>
       <button
