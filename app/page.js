043d1da9229bdca01802aa663a92e78ce42fc3ae
TonyChung7 <shihwei.chung@edu.sait.ca>
2026-03-13 19:07:08 -0600

    week-8: initial commit and update homepage

--- app/page.js
+++ app/page.js
@@ -1,67 +1,82 @@
 import Link from "next/link";
 
 export default function Home() {
+  const weeks = [2, 3, 4, 5, 6, 7, 8];
+  const linkStyle =
+    " hover:text-blue-700 hover:underline inline-block hover:translate-x-2 transition-all duration-300";
   return (
     <main>
       <h1 className="text-4xl font-extrabold p-6 mb-5 text-center">
         CPRG 306: Web Development 2 - Assignments
       </h1>
       <ul className="text-center text-lg space-y-3">
-        <li>
-          <Link
-            className=" hover:text-blue-700 hover:underline 
-            inline-block hover:translate-x-2 transition-all duration-300"
-            href="/week-2"
-          >
-            → Week 2 Assignment
-          </Link>
-        </li>
-        <li>
-          <Link
-            className=" hover:text-blue-700 hover:underline 
-            inline-block hover:translate-x-2 transition-all duration-300"
-            href="/week-3"
-          >
-            → Week 3 Assignment
-          </Link>
-        </li>
-        <li>
-          <Link
-            className=" hover:text-blue-700 hover:underline 
-            inline-block hover:translate-x-2 transition-all duration-300"
-            href="/week-4"
-          >
-            → Week 4 Assignment
-          </Link>
-        </li>
-        <li>
-          <Link
-            className=" hover:text-blue-700 hover:underline 
-            inline-block hover:translate-x-2 transition-all duration-300"
-            href="/week-5"
-          >
-            → Week 5 Assignment
-          </Link>
-        </li>
-        <li>
-          <Link
-            className=" hover:text-blue-700 hover:underline 
-            inline-block hover:translate-x-2 transition-all duration-300"
-            href="/week-6"
-          >
-            → Week 6 Assignment
-          </Link>
-        </li>
-        <li>
-          <Link
-            className=" hover:text-blue-700 hover:underline 
-            inline-block hover:translate-x-2 transition-all duration-300"
-            href="/week-7"
-          >
-            → Week 7 Assignment
-          </Link>
-        </li>
+        {weeks.map((week) => (
+          <li key={week}>
+            {" "}
+            <Link className={linkStyle} href={`/week-${week}`}>
+              {" "}
+              → Week {week} Assignment
+            </Link>{" "}
+          </li>
+        ))}
       </ul>
     </main>
   );
+}
+
+{
+  /* <li>
+  <Link
+    className=" hover:text-blue-700 hover:underline 
+    inline-block hover:translate-x-2 transition-all duration-300"
+    href="/week-2"
+  >
+    → Week 2 Assignment
+  </Link>
+</li>
+<li>
+  <Link
+    className=" hover:text-blue-700 hover:underline 
+    inline-block hover:translate-x-2 transition-all duration-300"
+    href="/week-3"
+  >
+    → Week 3 Assignment
+  </Link>
+</li>
+<li>
+  <Link
+    className=" hover:text-blue-700 hover:underline 
+    inline-block hover:translate-x-2 transition-all duration-300"
+    href="/week-4"
+  >
+    → Week 4 Assignment
+  </Link>
+</li>
+<li>
+  <Link
+    className=" hover:text-blue-700 hover:underline 
+    inline-block hover:translate-x-2 transition-all duration-300"
+    href="/week-5"
+  >
+    → Week 5 Assignment
+  </Link>
+</li>
+<li>
+  <Link
+    className=" hover:text-blue-700 hover:underline 
+    inline-block hover:translate-x-2 transition-all duration-300"
+    href="/week-6"
+  >
+    → Week 6 Assignment
+  </Link>
+</li>
+<li>
+  <Link
+    className=" hover:text-blue-700 hover:underline 
+    inline-block hover:translate-x-2 transition-all duration-300"
+    href="/week-7"
+  >
+    → Week 7 Assignment
+  </Link>
+</li> */
 }
